032d29bcc650876dab99f7dcc52d5f81b88f99b7
christian-iuil <okoriec01@gmail.com>
2024-05-23 10:33:38 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -26,135 +26,154 @@ class _MainAppState extends State<MainApp> {
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: PageView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        controller: pageCtrl,
-        itemCount: getList.length,
-        itemBuilder: (_, index) {
-          final data = getList[index];
-          return SizedBox(
-            height: size.height,
-            child: Stack(
-              fit: StackFit.passthrough,
-              children: [
-                Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      width: double.infinity,
-                      height: size.height * 0.6,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: CachedNetworkImageProvider(
-                                  data.backgroundImage))),
-                    )),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  top: size.height * 0.4 + 10,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
+          body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            flex: 11,
+            child: PageView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: pageCtrl,
+              itemCount: getList.length,
+              itemBuilder: (_, index) {
+                final data = getList[index];
+                return SizedBox(
+                  height: size.height,
+                  child: Stack(
+                    fit: StackFit.passthrough,
                     children: [
-                      CircleAvatar(
-                          backgroundColor: Colors.blue,
-                          radius: size.width * 0.2,
-                          backgroundImage:
-                              CachedNetworkImageProvider(data.foregroundImage)),
-                      SizedBox(height: size.height * 0.075),
-                      TextWidget(
-                          text: data.name,
-                          size: size.width * 0.1,
-                          letterSpacing: 1.7),
-                      TextWidget(
-                          text: data.serie,
-                          size: size.width * 0.050,
-                          letterSpacing: 1.7),
-                      SizedBox(height: size.height * 0.020),
-                      TextWidget(
-                        text: data.des,
-                        size: size.width * 0.040,
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: size.height * 0.010),
-                      Wrap(
-                        spacing: size.width * 0.020,
-                        children: [
-                          Chip(
-                              avatar: const Icon(Icons.shield_sharp),
-                              label: TextWidget(text: data.weapon, size: 12)),
-                          Chip(
-                              avatar: const Icon(Icons.female),
-                              label: TextWidget(text: data.wife, size: 12)),
-                          Chip(
-                              avatar: const Icon(Icons.moving_outlined),
-                              label: TextWidget(text: data.move, size: 12)),
-                        ],
-                      ),
-                      SizedBox(height: size.height * 0.050),
-                      SizedBox(
-                        width: size.width * 0.7,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            width: double.infinity,
+                            height: size.height * 0.6,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: CachedNetworkImageProvider(
+                                        data.backgroundImage))),
+                          )),
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        top: size.height * 0.4 + 10,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            GestureDetector(
-                                onTap: () {
-                                  if (currentIndex > 0) {
-                                    setState(() {
-                                      currentIndex--;
-                                      pageCtrl.animateToPage(
-                                        currentIndex,
-                                        duration:
-                                            const Duration(milliseconds: 300),
-                                        curve: Curves.bounceOut,
-                                      );
-                                    });
-                                  }
-                                },
-                                child: Icon(
-                                  Icons.arrow_back_ios,
-                                  size: size.width * 0.070,
-                                )),
-                            SmoothPageIndicator(
-                              controller: pageCtrl,
-                              count: getList.length,
-                              effect: JumpingDotEffect(
-                                  radius: size.width * 0.020,
-                                  dotHeight: size.height * 0.02,
-                                  dotWidth: size.width * 0.02),
+                            CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: size.width * 0.2,
+                                backgroundImage: CachedNetworkImageProvider(
+                                    data.foregroundImage)),
+                            SizedBox(height: size.height * 0.075),
+                            TextWidget(
+                                text: data.name,
+                                size: size.width * 0.1,
+                                letterSpacing: 1.7),
+                            TextWidget(
+                                text: data.serie,
+                                size: size.width * 0.050,
+                                letterSpacing: 1.7),
+                            SizedBox(height: size.height * 0.020),
+                            SizedBox(
+                              width: size.width * 0.8,
+                              child: TextWidget(
+                                text: data.des,
+                                size: size.width * 0.040,
+                                textAlign: TextAlign.center,
+                              ),
                             ),
-                            GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    if (currentIndex < getList.length - 1) {
-                                      setState(() {
-                                        currentIndex++;
-                                        pageCtrl.animateToPage(
-                                          currentIndex,
-                                          duration:
-                                              const Duration(milliseconds: 500),
-                                          curve: Curves.fastOutSlowIn,
-                                        );
-                                      });
-                                    }
-                                  });
-                                },
-                                child: Icon(Icons.arrow_forward_ios,
-                                    size: size.width * 0.070)),
+                            SizedBox(height: size.height * 0.030),
+                            Wrap(
+                              spacing: size.width * 0.020,
+                              children: [
+                                Chip(
+                                    avatar: const Icon(Icons.shield_sharp),
+                                    label: TextWidget(
+                                        text: data.weapon, size: 12)),
+                                Chip(
+                                    avatar: const Icon(Icons.female),
+                                    label:
+                                        TextWidget(text: data.wife, size: 12)),
+                                Chip(
+                                    avatar: const Icon(Icons.moving_outlined),
+                                    label:
+                                        TextWidget(text: data.move, size: 12)),
+                              ],
+                            ),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
+          ),
+          Flexible(
+            flex: 1,
+            child: SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            if (currentIndex > 0) {
+                              setState(() {
+                                currentIndex--;
+                                pageCtrl.animateToPage(
+                                  currentIndex,
+                                  duration: const Duration(milliseconds: 300),
+                                  curve: Curves.fastLinearToSlowEaseIn,
+                                );
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            size: size.width * 0.070,
+                          )),
+                      SmoothPageIndicator(
+                        controller: pageCtrl,
+                        count: getList.length,
+                        effect: JumpingDotEffect(
+                            radius: size.width * 0.020,
+                            dotHeight: size.height * 0.02,
+                            dotWidth: size.width * 0.02),
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (currentIndex < getList.length - 1) {
+                                setState(() {
+                                  currentIndex++;
+                                  pageCtrl.animateToPage(
+                                    currentIndex,
+                                    duration: const Duration(milliseconds: 500),
+                                    curve: Curves.fastOutSlowIn,
+                                  );
+                                });
+                              }
+                            });
+                          },
+                          child: Icon(Icons.arrow_forward_ios,
+                              size: size.width * 0.070)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       )),
     );
   }
 }
-
-
