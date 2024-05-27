37560db2eca77265b97148df3ca74c7cc58a3570
christian-iuil <okoriec01@gmail.com>
2024-05-27 18:35:52 +0100

    update

---
+++ lib/virw/slider_ii.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SliderTwo extends StatelessWidget {
+  const SliderTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
